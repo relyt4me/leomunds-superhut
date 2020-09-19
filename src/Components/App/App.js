ebf37bdf8dd80260511fe5a11de1fd0ed625a350
Tyler Haglund <relyt4me@gmail.com>
2020-09-19 11:14:26 -0600

    Import subcomponents

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Header from '../Header/Header';
+import CategoriesDisplay from '../CategoriesDisplay/CategoriesDisplay';
+import ErrorPage from '../ErrorPage/ErrorPage';
+import SingleCategoryDisplay from '../SingleCategoryDisplay/SingleCategoryDisplay';
+import Cart from '../Cart/Cart';
+// import SingleItemDisplay from '../SearchItemDisplay/SearchItemDisplay'
+import { Route } from 'react-router-dom';
 
-function App() {
-  return <div className='App'></div>;
+class APP extends Component {
+  constructor() {
+    super();
+    this.state = {
+      cartItems: [],
+      error: '',
+    };
+  }
+
+  render() {
+    return <div className='App'></div>;
+  }
 }
 
 export default App;
+
+/**
+ * APP
+ *  Header * Search/ Page Title/ Back to Store/ Cart
+ *  (CategoriesDisplay) -Cards
+ *  (ErrorPage)
+ *  (Cart)
+ *  (ItemDisplay)- cards
+ */
