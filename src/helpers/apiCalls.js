acb576bb87ef378128b48ba8c66718c435bd5a4a
Tyler Haglund <relyt4me@gmail.com>
2020-09-19 15:29:01 -0600

    Add call for getItemsInCategory

--- src/helpers/apiCalls.js
+++ src/helpers/apiCalls.js
@@ -9,3 +9,15 @@ export const getCategories = () => {
     })
     .then((categories) => categories.results);
 };
+
+export const getItemsInCategory = (categoryId) => {
+  return fetch(`https://www.dnd5eapi.co/api/equipment-categories/${categoryId}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw Error(response.statusText);
+      } else {
+        return response.json();
+      }
+    })
+    .then((category) => category.equipment);
+};
