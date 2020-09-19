57624bf2cc4ea7fb751e40cc8fcfa6645d2fcadb
Tyler Haglund <relyt4me@gmail.com>
2020-09-19 12:13:09 -0600

    Render title and two buttons

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import './Header.css';
 import PropTypes from 'prop-types';
+import horseAndCart from '../../assets/horse-cart.png';
 
-const Header = () => {
-  return <section className='header'>Header</section>;
+const Header = ({ pageTitle, cart }) => {
+  return (
+    <header>
+      <button className='return-to-storefront-button'>Return to Storefront</button>
+      <h1 className='page-title'>{pageTitle}</h1>
+      <button className='my-cart-button'>
+        My Cart {cart.length}
+        <img src={horseAndCart} className='horse-cart-img' alt={`Horse pulling a cart`} />
+      </button>
+    </header>
+  );
 };
 
 export default Header;
 
 Header.propTypes = {
-  // logoutUser: PropTypes.func
+  pageTitle: PropTypes.string,
+  cart: PropTypes.array,
 };
