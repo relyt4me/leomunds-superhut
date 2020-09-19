19cdf5547f35a457f7e82c219588987d5b8a4260
Tyler Haglund <relyt4me@gmail.com>
2020-09-19 15:15:47 -0600

    Create test for render of Welcome component

---
+++ src/Components/Welcome/Welcome.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Welcome from './Welcome';
+import { screen, render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+
+describe('Welcome Component', () => {
+  it('Should have the correct content when rendered', () => {
+    render(
+      <MemoryRouter>
+        <Welcome />
+      </MemoryRouter>
+    );
+
+    const welcomeTitle = screen.getByRole('heading', { name: "Welcome to Leomund's Superhut" });
+    // const welcomeMessage = screen.getByText();
+    const getStartedButton = screen.getByRole('button', { name: 'Get Started at the Storefront' });
+
+    expect(welcomeTitle).toBeInTheDocument();
+    // expect(welcomeMessage).toBeInTheDocument();
+    expect(getStartedButton).toBeInTheDocument();
+  });
+});
