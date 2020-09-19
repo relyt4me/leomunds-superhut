e1cc647cf49438bcef5cfc9f2c2919c95801334d
Tyler Haglund <relyt4me@gmail.com>
2020-09-19 11:24:04 -0600

    Add routes to all subcomponents

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from '../Header/Header';
+import Welcome from '../Welcome/Welcome';
 import CategoriesDisplay from '../CategoriesDisplay/CategoriesDisplay';
 import ErrorPage from '../ErrorPage/ErrorPage';
 import SingleCategoryDisplay from '../SingleCategoryDisplay/SingleCategoryDisplay';
@@ -8,7 +9,7 @@ import Cart from '../Cart/Cart';
 // import SingleItemDisplay from '../SearchItemDisplay/SearchItemDisplay'
 import { Route } from 'react-router-dom';
 
-class APP extends Component {
+class App extends Component {
   constructor() {
     super();
     this.state = {
@@ -18,7 +19,42 @@ class APP extends Component {
   }
 
   render() {
-    return <div className='App'></div>;
+    return (
+      <div className='App'>
+        <Header />
+        <Route
+          exact
+          path='/'
+          render={() => {
+            return <Welcome />;
+          }}
+        />
+        <Route
+          path='/storefront'
+          render={() => {
+            return <CategoriesDisplay />;
+          }}
+        />
+        <Route
+          path='/error'
+          render={() => {
+            return <ErrorPage />;
+          }}
+        />
+        <Route
+          path='/:category/items'
+          render={() => {
+            return <SingleCategoryDisplay />;
+          }}
+        />
+        <Route
+          path='/cart'
+          render={() => {
+            return <Cart />;
+          }}
+        />
+      </div>
+    );
   }
 }
 
@@ -27,6 +63,7 @@ export default App;
 /**
  * APP
  *  Header * Search/ Page Title/ Back to Store/ Cart
+ *  (Welcome)
  *  (CategoriesDisplay) -Cards
  *  (ErrorPage)
  *  (Cart)
