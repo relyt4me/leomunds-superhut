54f918da27fc72b264e1dd36ba983213061f189f
Tyler Haglund <relyt4me@gmail.com>
2020-09-19 21:10:53 -0600

    Add basic layout to SingleCategoryDisplay

--- src/Components/SingleCategoryDisplay/SingleCategoryDisplay.js
+++ src/Components/SingleCategoryDisplay/SingleCategoryDisplay.js
@@ -1,13 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './SingleCategoryDisplay.css';
+import { getCategories } from '../../helpers/apiCalls';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import ItemCard from '../ItemCard/ItemCard;
 
-const SingleCategoryDisplay = () => {
-  return <section className='single-category-display'>SingleCategoryDisplay</section>;
-};
+class SingleCategoryDisplay extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      itemsInCategory: [],
+    };
+  }
+
+  createCards() {
+    return this.state.itemsInCategory.map((item) => {
+      return (
+          <ItemCard item={item} />
+      );
+    });
+  }
+
+  componentDidMount() {
+    getCategories()
+      .then((results) => {
+        this.setState({ categories: results });
+      })
+      .catch((error) => {
+        this.props.setError('Vecna has attacked out stores!! Please come back after an adventurer cleans this up.');
+      });
+  }
+
+  render() {
+    const isLoading = !this.state.categories.length;
+    return (
+      <section className='categories-display'>
+        {isLoading && <h1>Loading...</h1>}
+        {!isLoading && this.createCards()}
+      </section>
+    );
+  }
+}
 
 export default SingleCategoryDisplay;
 
 SingleCategoryDisplay.propTypes = {
-  // logoutUser: PropTypes.func
+  setError: PropTypes.func,
 };
+
