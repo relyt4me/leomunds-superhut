7c950082c3b1b50469a2f92eb35e3cb1fc1353a6
Tyler Haglund <relyt4me@gmail.com>
2020-09-19 21:22:36 -0600

    Add key to map

--- src/Components/SingleCategoryDisplay/SingleCategoryDisplay.js
+++ src/Components/SingleCategoryDisplay/SingleCategoryDisplay.js
@@ -14,7 +14,7 @@ class SingleCategoryDisplay extends Component {
 
   createCards() {
     return this.state.itemsInCategory.map((item) => {
-      return <ItemCard item={item} setError={this.props.setError} />;
+      return <ItemCard item={item} setError={this.props.setError} key={item.index} />;
     });
   }
 
