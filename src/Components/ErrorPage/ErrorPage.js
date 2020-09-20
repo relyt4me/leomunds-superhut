c160579b48502d1f0eb6b56e006f0d736a4fd62c
Tyler Haglund <relyt4me@gmail.com>
2020-09-20 12:41:20 -0600

    Add bad dice roll icon

--- src/Components/ErrorPage/ErrorPage.js
+++ src/Components/ErrorPage/ErrorPage.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './ErrorPage.css';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import criticalFail from '../../assets/critical-fail.jpg';
 
 const ErrorPage = ({ error, setError }) => {
   return (
     <section className='page-error'>
+      <img src={criticalFail} className='critical-fail-icon' alt={`Red 20 sided dice set on fire with a rolled number of 1`} />
       <h2>{error}</h2>
       <Link to={'/'}>
         <button className='return-home-button' onClick={() => setError('')}>
