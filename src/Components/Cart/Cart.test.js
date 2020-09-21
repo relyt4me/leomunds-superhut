24d6bff7273459a381819952b76c5e572492fc2c
Tyler Haglund <relyt4me@gmail.com>
2020-09-20 20:20:04 -0600

    Add clearCart test

--- src/Components/Cart/Cart.test.js
+++ src/Components/Cart/Cart.test.js
@@ -45,6 +45,60 @@ describe('Cart Component', () => {
     expect(modifiedTotal).toBeInTheDocument();
   });
 
+  it('Should modify the price as the slider changes', () => {
+    const mockCart = [{ currency: 'gp', cost: 100, name: 'Breastplate', index: 'breastplate' }];
+
+    render(
+      <MemoryRouter>
+        <Cart cartItems={mockCart} />
+      </MemoryRouter>
+    );
+
+    const priceModifySlider = screen.getByRole('slider');
+
+    fireEvent.change(priceModifySlider, { target: { value: '0.1' } });
+
+    const priceModifyLabel = screen.getByText('Price Modify: 10 %');
+    const modifiedTotal = screen.getByRole('heading', { name: 'Modified Total: 110 gp, 0 sp, 0 cp' });
+
+    expect(priceModifyLabel).toBeInTheDocument();
+    expect(modifiedTotal).toBeInTheDocument();
+  });
+
+  it('Should call clearCart when the clear button is clicked', () => {
+    const mockClearCart = jest.fn();
+
+    render(
+      <MemoryRouter>
+        <Cart clearCart={mockClearCart} cartItems={[]} />
+      </MemoryRouter>
+    );
+
+    const clearCartButton = screen.getByRole('button', { name: 'Clear Cart' });
+
+    fireEvent.click(clearCartButton);
+
+    expect(mockClearCart).toBeCalledTimes(1);
+  });
+
+  it('Should call removeItem when the clear button is clicked', () => {
+    const mockCart = [{ currency: 'gp', cost: 100, name: 'Breastplate', index: 'breastplate' }];
+    const mockRemoveItem = jest.fn();
+
+    render(
+      <MemoryRouter>
+        <Cart cartItems={mockCart} removeItem={mockRemoveItem} />
+      </MemoryRouter>
+    );
+
+    const removeButton = screen.getByRole('button', { name: 'Remove' });
+
+    fireEvent.click(removeButton);
+
+    expect(mockRemoveItem).toBeCalledTimes(1);
+    expect(mockRemoveItem).toBeCalledWith('breastplate');
+  });
+
   // it('Should show a stand in card while the fetch calls', () => {
   //   const mockArrow = {
   //     index: 'arrow',
