2c9b4566490ea83e00187f26ce731acaad426b30
Tyler Haglund <relyt4me@gmail.com>
2020-09-21 11:47:46 -0600

    Change nulls to rejected values

--- src/Components/Cart/Cart.test.js
+++ src/Components/Cart/Cart.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Cart from './Cart';
-import { screen, render, waitFor, fireEvent } from '@testing-library/react';
+import { screen, render, fireEvent } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import '@testing-library/jest-dom';
 
