fb2f99dafef4f58b0f3316b60c251c49c469c3af
Tyler Haglund <relyt4me@gmail.com>
2020-09-21 13:47:28 -0600

    Add test for adding an item through search

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -138,4 +138,62 @@ describe('Integration testing', () => {
     expect(adventureCard).toBeInTheDocument();
     expect(newPageTitle).toBeInTheDocument();
   });
+
+  it('should allow a user to add a searched item to their cart', async () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+
+    const getStartedButton = await waitFor(() => screen.getByRole('button', { name: 'Get Started at the Storefront' }));
+
+    fireEvent.click(getStartedButton);
+
+    const searchInput = await waitFor(() => screen.getByRole('textbox'));
+    const searchButton = screen.getByRole('button', { name: 'Find' });
+
+    fireEvent.change(searchInput, { target: { value: 'Arrow' } });
+    fireEvent.click(searchButton);
+
+    const addItemButton = await waitFor(() => screen.getByRole('button', { name: 'Add to Cart Item floating in hand' }));
+
+    fireEvent.click(addItemButton);
+
+    const cartButton = screen.getByRole('button', { name: 'My Cart Horse pulling a cart 1' });
+
+    expect(cartButton).toBeInTheDocument();
+
+    fireEvent.click(cartButton);
+
+    const arrowItem = screen.getByText('Sword');
+    const removeButton = screen.getByRole('button', { name: 'Remove' });
+
+    expect(arrowItem).toBeInTheDocument();
+    expect(removeButton).toBeInTheDocument();
+  });
+
+  it('should allow a user to add items to the cart from the category page and remove them', async () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+
+    const getStartedButton = await waitFor(() => screen.getByRole('button', { name: 'Get Started at the Storefront' }));
+
+    fireEvent.click(getStartedButton);
+
+    const adventureCard = await waitFor(() => screen.getByRole('heading', { name: 'Adventuring Gear' }));
+
+    fireEvent.click(adventureCard);
+
+    const arrowCardAddButton = await waitFor(() => screen.getAllByRole('button', { name: 'Add to Cart Item floating in hand' })[0]);
+    const swordCardAddButton = screen.getAllByRole('button', { name: 'Add to Cart Item floating in hand' })[0];
+
+    fireEvent.click(arrowCardAddButton);
+    fireEvent.click(swordCardAddButton);
+
+    const 
+  });
 });
