2b6499159d48759e4c90f8ea0e8ff9795c55c8ff
Tyler Haglund <relyt4me@gmail.com>
2020-09-21 12:42:34 -0600

    Pass initial render

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,5 +1,20 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { screen, render, waitFor, fireEvent } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-it('pass', () => {});
+describe('App Component', () => {
+  it('should load with the welcome page and header', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+
+    const headerTitle = screen.getByRole('heading', { name: "Leomund's Superhut" });
+    const welcomeHeader = screen.getByRole('heading', { name: "Welcome to Leomund's Superhut" });
+
+    expect(headerTitle).toBeInTheDocument();
+    expect(welcomeHeader).toBeInTheDocument();
+  });
+});
